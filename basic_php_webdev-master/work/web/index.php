1819ede8c63a029b7707d6c728d129824211219a
skatedog <skatedog0001@gmail.com>
2021-12-13 14:39:50 +0900

    [Finish]ドットインストール_詳解PHP ウェブ開発編

--- basic_php_webdev-master/work/web/index.php
+++ basic_php_webdev-master/work/web/index.php
@@ -2,16 +2,40 @@
 
 require('../app/functions.php');
 
+createToken();
+
+define('FILENAME', '../app/messages.txt');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  validateToken();
+
+  $message = trim(filter_input(INPUT_POST, 'message'));
+  $message = $message !== '' ? $message : '...';
+  
+  $fp = fopen(FILENAME, 'a');
+  fwrite($fp, $message . "\n");
+  fclose($fp);
+
+  header('Location: http://localhost:8080/result.php');
+  exit;
+}
+
+$messages = file(FILENAME, FILE_IGNORE_NEW_LINES);
+
 include('../app/_parts/_header.php');
 
 ?>
 
-<form action="result.php" method="get">
-  <label><input type="radio" name="color" value="orange">Orange</label>
-  <label><input type="radio" name="color" value="pink">Pink</label>
-  <label><input type="radio" name="color" value="gold">Gold</label>
-  <button>Send</button>
-  <a href="reset.php">[reset]</a>
+<ul>
+<?php foreach ($messages as $message): ?>
+  <li><?= h($message); ?></li>
+<?php endforeach; ?>
+</ul>
+
+<form action="" method="post">
+  <input type="text" name="message">
+  <button>Post</button>
+  <input type="hidden" name="token" value="<?= h($_SESSION['token']) ?>">
 </form>
 
 <?php
